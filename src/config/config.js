271eed8b09444abd6789112b81d661c8aa410033
Teptat <miind.tsktt@gmail.com>
2022-11-10 17:33:07 +0700

    Last commit

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 export const firebaseConfig = {
-    
+    apiKey: "AIzaSyB0Pao-W6I2kKi9TidRw5WMAdk-zcDV1Hc",
+    authDomain: "handy-5f4ba.firebaseapp.com",
+    projectId: "handy-5f4ba",
+    storageBucket: "handy-5f4ba.appspot.com",
+    messagingSenderId: "17567610683",
+    appId: "1:17567610683:web:47accbd65905b37b9f691e"
   };
 
 const app = initializeApp(firebaseConfig);
